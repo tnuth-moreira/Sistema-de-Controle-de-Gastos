41b5a1f29513152ba989749355bb9633a83b6d49
Lucas Waltz <lucaswaltz7@gmail.com>
2023-09-04 18:18:41 -0300

    Começando Projeto

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./Login/styles.css";
+import BackgroundImage from "./assets/background.png"
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+
+
+    <img src={BackgroundImage} />
+
+    <div className="container-main">
+
+      <div className="adver-text">
+
+        Controle suas <span>finanças</span> , sem planilha chata.
+
+      </div>
+
+
+      <div className="login-form">
+
+        <h1>Login</h1>
+        <form>
+
+        </form>
+      </div>
+
+
+    </div>
+
+
   </React.StrictMode>
 );
